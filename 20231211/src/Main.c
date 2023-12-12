a4071abd058f1e68cc462b4d9f766cc937a78811
Joan Francesc Pedro Garcia <joan.f.pedro@protonmail.com>
2023-12-12 18:22:27 +1100

    Updated README problems with status solved / not solved

---
+++ 20231211/src/Main.c
@@ -0,0 +1,16 @@
+/*
+ * Main.c
+ * 
+ * Description: Main file of the program
+ * 
+ * Author: Joan Francesc Pedro Garcia
+ * Date: 07-12-2023
+ */
+
+#include "Utils.h"
+
+int main (int argc, char *argv[])
+{
+	printf("Hello world!\n");
+	return 0;
+}
