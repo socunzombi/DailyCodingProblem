cbc1d645d34aadab5eba27bac97cd95f8b011dba
Joan Francesc Pedro Garcia <joan.f.pedro@protonmail.com>
2023-12-12 18:17:27 +1100

    Solution problem 10-12-2023

--- 20231210/src/Main.c
+++ 20231210/src/Main.c
@@ -11,6 +11,46 @@
 
 int main (int argc, char *argv[])
 {
-	printf("Hello world!\n");
+    // We check that the user gives us values
+	if (argc < 3) {
+		printf("Usage: %s <arrayValue1> <arrayValue2> ... <arrayValueN>\n", argv[0]);
+		return 1;
+	}
+
+
+	// We set the variables with the parameters
+	int arraySize = argc - 1;
+	int values[arraySize];
+
+	// We add all numbers to the array
+	for (int n = 0; n < arraySize; n++)
+	{
+		values[n] = atoi(argv[n+1]);
+	}
+
+	printf("Lowest value: %d\n", lowestPositiveValue(values, arraySize));
+	return 0;
+}
+
+int lowestPositiveValue(int values[], int arraySize)
+{
+	int numbersViewed[10000] = {0};
+
+	for (int position = 0; position < arraySize; position++)
+	{
+		if (values[position] > 0)
+		{
+			numbersViewed[values[position]] = 1;
+		}
+	}
+
+	for (int minValue = 1; minValue < 10000; minValue++)
+	{
+		if (numbersViewed[minValue] == 0)
+		{
+			return minValue;
+		}
+	}
+
 	return 0;
 }
