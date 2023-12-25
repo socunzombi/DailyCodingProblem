8ff605342761160482bd7b374a75b129d22c8f0a
Joan Francesc Pedro Garcia <joan.f.pedro@protonmail.com>
2023-12-25 11:46:39 +1100

    New folders gitignore

---
+++ 20231212/include/Utils.h
@@ -0,0 +1,16 @@
+/*
+ * Utils.h
+ *
+ * Description: This file contains all the includes and functions declarations.
+ *
+ * Author: Joan Francesc Pedro Garcia
+ * Date: 07-12-2023
+ */
+
+// region include
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+// endregion include
+
+// Functions
